a53638509e3abdf31f44ea48fed38b1ddd50d90d
Tom Crayford <tcrayford@googlemail.com>
2011-03-19 18:20:15 +0000

    Store field names in VALUE_ATTRS.

--- lib/values.rb
+++ lib/values.rb
@@ -2,15 +2,17 @@ class Value
   def self.new(*fields)
     return Class.new do
       attr_reader *fields
+
       define_method(:initialize) do |*input_fields|
         raise ArgumentError.new("wrong number of arguments, #{input_fields.size} for #{fields.size}") if fields.size != input_fields.size
 
         fields.each_with_index do |field, index|
           instance_variable_set('@' + field.to_s, input_fields[index])
         end
-        @__fields = fields
         self.freeze
       end
+
+      const_set :VALUE_ATTRS, fields
 
       def ==(other)
         self.eql?(other)
@@ -19,7 +21,7 @@ class Value
       def eql?(other)
         return false if other.class != self.class
         result = true
-        @__fields.each do |field|
+        self.class::VALUE_ATTRS.each do |field|
           result = result && self.send(field) == other.send(field)
         end
         return result
@@ -27,7 +29,7 @@ class Value
 
       def hash
         result = 0
-        @__fields.each do |field|
+        self.class::VALUE_ATTRS.each do |field|
           result += field.hash
         end
         return result + self.class.hash
