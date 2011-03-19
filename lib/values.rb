d588402dac192e29f887dc5929f9ce9af1918a07
Aviv Ben-Yosef <aviv.by@gmail.com>
2011-03-19 19:13:37 +0200

    Use Class.new block instead of instance_eval

--- lib/values.rb
+++ lib/values.rb
@@ -1,7 +1,6 @@
 class Value
   def self.new(*fields)
-    value_class = Class.new
-    value_class.instance_eval do
+    return Class.new do
       attr_reader *fields
       define_method(:initialize) do |*input_fields|
         raise ArgumentError.new("wrong number of arguments, #{input_fields.size} for #{fields.size}") if fields.size != input_fields.size
@@ -12,6 +11,5 @@ class Value
         self.freeze
       end
     end
-    return value_class
   end
 end
