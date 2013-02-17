52823d1840eb2d2810f2f9868f0baf49eecddd4b
Tom Stuart <tom@codon.com>
2013-02-17 20:11:19 +0000

    Use string interpolation in #instance_variable_set

--- lib/values.rb
+++ lib/values.rb
@@ -7,7 +7,7 @@ class Value
         raise ArgumentError.new("wrong number of arguments, #{input_fields.size} for #{fields.size}") if fields.size != input_fields.size
 
         fields.each_with_index do |field, index|
-          instance_variable_set('@' + field.to_s, input_fields[index])
+          instance_variable_set("@#{field}", input_fields[index])
         end
         self.freeze
       end
