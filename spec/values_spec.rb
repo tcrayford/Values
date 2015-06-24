d1580aa91ec490fd3470ffef86bce2d57484dede
Marc Siegel <marc@usainnov.com>
2015-06-24 12:37:06 -0400

    Use spec_helper.rb in specs
    The existing spec_helper.rb was not used in the specs
    file. This commit uses it. Currently, all that it does
    is require the library.

--- spec/values_spec.rb
+++ spec/values_spec.rb
@@ -1,5 +1,5 @@
-require File.expand_path(File.dirname(__FILE__) + '/../lib/values')
-describe 'values' do
+require 'spec_helper'
+
   it 'raises argument error if given zero fields' do
     expect { Value.new }.to raise_error(ArgumentError, 'wrong number of arguments (0 for 1+)')
   end
